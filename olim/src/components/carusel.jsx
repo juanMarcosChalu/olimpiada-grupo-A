7b46f7eeb6927a3541c07a9f12ad91194d85a4f6
Chalu Juan <chalujuanmarcos@gmail.com>
2025-06-10 22:46:57 -0300

    1.0.2
    terminar carusel, añadir iconos de las flechas, hover, agregar hook personalizado para el fetch

--- olim/src/components/carusel.jsx
+++ olim/src/components/carusel.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { useFetch } from '../useFetch';
+
 import img1 from "../assets/disney.jpg";
 import img2 from "../assets/costarica.jpg";
 import img3 from "../assets/madrid.jpg";
@@ -7,37 +9,69 @@ import img5 from "../assets/paris.jpg";
 import "./Carousel.css";
 
 const Carousel = () => {
+    const { data, loading, eror } = useFetch("https://pokeapi.co/api/v2/pokemon?limit=10&offset=0");
     const images = [img1, img2, img3, img4, img5];
+    const imagene1 ={
+    img: img1,  // ¡La variable que exportaste!
+    texto: "Disney"
+    }
+    const imagene2 ={
+    img: img2,  // ¡La variable que exportaste!
+    texto: "Costa Rica"
+    }
+    const imagene3 ={
+    img: img3,  // ¡La variable que exportaste!
+    texto: "Madrid"
+    }
+    const imagene4 ={
+    img: img4,  // ¡La variable que exportaste!
+    texto: "New York"
+    }
+    const imagene5 ={
+    img: img5,  // ¡La variable que exportaste!
+    texto: "Paris"
+    }
+    const imagenes=[imagene1,imagene2,imagene3,imagene4,imagene5];
+    
     const [startIndex, setStartIndex] = useState(0);
 
     const nextSlide = () => {
-        setStartIndex((prevIndex) => (prevIndex + 1) % images.length);
+        setStartIndex((prevIndex) => (prevIndex + 1) % imagenes.length);
     };
 
     const prevSlide = () => {
-        setStartIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+        setStartIndex((prevIndex) => (prevIndex - 1 + imagenes.length) % imagenes.length);
     };
 
     // Obtenemos los 3 elementos a mostrar, con wrap-around (circular)
     const visibleImages = [];
     for (let i = 0; i < 5; i++) {
-        visibleImages.push(images[(startIndex + i) % images.length]);
+        visibleImages.push(imagenes[(startIndex + i) % images.length]);
     }
 
     return (
         <div className="carousel-wrapper">
             <h2 className="carousel-title">Viajes que inspiran, experiencias que perduran</h2>
             <div className="carousel">
-                <button onClick={prevSlide}>aca</button>
+                <button onClick={prevSlide}>
+                    <span class="material-symbols-outlined left">
+                        navigation
+                    </span>
+                </button>
 
                 <div className="carousel-container">
-                    {visibleImages.map((imgSrc, index) => (
+                    {visibleImages.map((imagen, index) => (
                         <div className="carousel-slide" key={index}>
-                            <img src={imgSrc} alt={`Slide ${index}`} />
+                            <img src={imagen.img} alt={`Slide ${index}`} />
+                            <span>{imagen.texto}</span>
                         </div>
                     ))}
                 </div>
-                <button onClick={nextSlide}>aca</button>
+                <button onClick={nextSlide}>
+                    <span class="material-symbols-outlined">
+                        navigation
+                    </span>
+                </button>
             </div>
         </div>
 
---
+++ olim/src/useFetch.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+import React from "react";
+
+export function useFetch(url) {
+    const [data,setData] = useState(null);
+    const [loading,setLoading]= useState(true);
+    const [error,setErorr]= useState(null);
+    useEffect(()=>{
+        setLoading(true);
+        fetch(url)
+        .then((response)=>response.json())
+        .then((data)=>setData(data))
+        .catch((error)=>setErorr(error))
+        .finally(()=>setLoading(false));
+    },[])
+    return {data,loading,error};
+}
