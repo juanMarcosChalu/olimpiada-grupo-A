387aa9d10c605c8ad29d651f07e8215ae676f536
juanMarcosChalu <164274955+juanMarcosChalu@users.noreply.github.com>
2025-06-21 15:43:56 -0300

    1.2.3
    responsive header y otras cositas

--- olim/src/components/Layouts/header.jsx
+++ olim/src/components/Layouts/header.jsx
@@ -5,30 +5,50 @@ import { Link } from "react-router-dom";
 import ServiciosMenu from "../UI/serviciosli.jsx";
 import UsuarioMenu from '../UI/UserMenu.jsx';
 import SvgCarrito from '../UI/SvgCarrito.jsx';
-function Header({ title, menuItems = [],backgroundColorProp }) {
+function Header({ title, menuItems = [], backgroundColorProp }) {
 
   const [userOpen, setUserOpen] = useState(false);
- 
+
   return (
     <header className={styles.header} style={{ backgroundColor: backgroundColorProp }}>
       <div className={styles.logocontainer}>{title}</div>
       <nav className={styles.navigation}>
         <ul className={styles.navigationList}>
-         {menuItems.map((item, index) =>
-         
-    item.key === "servicios" ? (
-     
-      // Este ya devuelve <li>, no lo envuelvas en otro
-      <ServiciosMenu key={index} />
-    ) : (
-      // Este sí necesita <li> si es un <Link> o similar
-      <li key={index} className={styles.navbuttons}>{item}</li>
-    )
-  )}
+          {menuItems.map((item, index) =>
+
+            item.key === "servicios" ? (
+
+              // Este ya devuelve <li>, no lo envuelvas en otro
+                <li className={styles.navbarPc}> <ServiciosMenu key={index}  /></li>
+             
+            ) : (
+              // Este sí necesita <li> si es un <Link> o similar
+              <li key={index} className={`${styles.navbuttons} ${styles.navbarPc}`}>{item}</li>
+            )
+          )}
 
           {/* Icono usuario fijo al final */}
-          <SvgCarrito />
-          <UsuarioMenu></UsuarioMenu>
+          <li className={styles.navbarPc}>
+            <SvgCarrito />
+          </li>
+          <li className={styles.navbarPc}>
+            <UsuarioMenu />
+          </li>
+          <div
+            className={`${styles.hamburguesa} ${userOpen ? styles.open : ""}`}
+            onClick={() => setUserOpen(!userOpen)}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+
+          <div className={`${styles.navbarLinks} ${userOpen ? styles.active : ""}`}>
+            <a href="#inicio">Inicio</a>
+            <a href="#servicios">Servicios</a>
+            <a href="#proyectos">Proyectos</a>
+            <a href="#contacto">Contacto</a>
+          </div>
         </ul>
       </nav>
     </header>
