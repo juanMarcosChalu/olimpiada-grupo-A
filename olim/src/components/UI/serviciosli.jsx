ce1ae71d3250a5a37043c1b582296c9fd7810a5e
juanMarcosChalu <164274955+juanMarcosChalu@users.noreply.github.com>
2025-06-24 16:51:01 -0300

    1.3.7
    cambio en el menu de servicviso en el home no se qe decir

--- olim/src/components/UI/serviciosli.jsx
+++ olim/src/components/UI/serviciosli.jsx
@@ -3,25 +3,46 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "../../styles/header2.module.css";
 
+
 function ServiciosMenu() {
   const [open, setOpen] = useState(false);
+   
   document.addEventListener("click", (e) => {
     if (!e.target.closest(`.${styles.navbuttons}`)) {
       setOpen(false);
     }
   });
   return (
-    <div className={open ? styles.fondoActivo : styles.fondodesactivo} key="Servicios">
+    <div key="Servicios" >
       <button className={styles.navbuttons} onClick={() => setOpen(!open)}>
         Servicios
       </button>
-      <ul className={`${styles.submenu} ${open ? styles.opacityon : styles.opacityoff}`}>
-        <li><Link to="/vuelos">Vuelos</Link></li>
-        <li><Link to="/alquileres">Alquileres de auto</Link></li>
-        <li><Link to="/alojamiento">Alojamiento</Link></li>
-        <li><Link to="/paquetes">Paquetes</Link></li>
-        <li><Link to="/asistencia">Asistencia al Viajero</Link></li>
-      </ul>
+  
+       {open && (
+                      <div className={styles.userBox}>
+                          
+      
+                        
+                          <ul className={styles.options}>
+                              <Link to="/perfil">
+                                  <li><span className={`${styles.icon} material-symbols-outlined`}>person</span>Vuelos</li>
+                              </Link>
+                              <Link to="/Favoritos">
+                                  <li><span className={`${styles.icon} material-symbols-outlined`}>favorite</span>Alquileres de auto</li>
+                              </Link>
+                              <Link to="/Notificaciones">
+                                  <li><span className={`${styles.icon} material-symbols-outlined`}>notifications</span>alojamiento</li>
+                              </Link>
+                              <Link to="/Notificaciones">
+                                  <li><span className={`${styles.icon} material-symbols-outlined`}>notifications</span>Paquetes</li>
+                              </Link>
+                              <Link to="/Notificaciones">
+                                  <li><span className={`${styles.icon} material-symbols-outlined`}>notifications</span>Asistencia al Viajero</li>
+                              </Link>
+                          </ul>
+                          {/* <button className={styles.logout}>Cerrar Sesión</button> */}
+                      </div>
+                  )}
     </div>
   );
 }
